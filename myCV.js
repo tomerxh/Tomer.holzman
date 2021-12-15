af9b9c5ffb741f63c252381685bffb0f2e9fc444
tomerxh <93145898+tomerxh@users.noreply.github.com>
2021-12-15 22:00:56 +0200

    Add files via upload

--- myCV.js
+++ myCV.js
@@ -5,7 +5,7 @@ function checkIfGood(){
         phMsg = "Thank you";
     }
     else{
-        phMsg = "Pattern should be XXX@XXXX.com";
+        phMsg = "Pattern should be 05XXXXXXX";
     }
     document.getElementById("phoneV").innerHTML = phMsg;
 
@@ -15,7 +15,7 @@ function checkIfGood(){
         emailMsg = "Thank you";
     }
     else{
-        emailMsg = "lease try again";
+        emailMsg = "Pattern should be XXX@XXXX.com"
     }
     document.getElementById("emailVM").innerHTML = emailMsg;
 
@@ -24,24 +24,3 @@ function checkIfGood(){
     }
 }
 
-function next(){
-    alert("MazalTOV")
-}
-
- /* Time  */
-
-function startTime() {
-  const today = new Date();
-  let h = today.getHours();
-  let m = today.getMinutes();
-  let s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('Clock').innerHTML =  h + ":" + m + ":" + s;
-  setTimeout(startTime, 1000);
-}
-
-function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-  return i;
-}}
